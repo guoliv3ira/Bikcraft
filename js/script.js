27b97e8ba478f3488cd67208266713f78f127d21
Gustavo Oliveira <55859418+guoliv3ira@users.noreply.github.com>
2020-08-12 08:30:48 -0300

    formulario

--- js/script.js
+++ js/script.js
@@ -26,3 +26,12 @@ hamburger.addEventListener('click', ()=>{
     menuContainer.setAttribute("data-anime","200")
     
 })
+
+if(window.SimpleForm) {
+	new SimpleForm({
+	  form: ".formphp", // seletor do formulário
+	  button: "#enviar", // seletor do botão
+	  erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // mensagem de erro
+	  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+	});
+}
